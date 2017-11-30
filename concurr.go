23b8453e5980ac22b6286a39f3b4ce351f4a7173
Josep Subils <js@js.gl>
2017-11-30 13:41:17 +0100

    [add] first file

---
+++ concurr.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	messages := make(chan string, 2)
+
+	go f("defer", messages)
+	go f("defer2", messages)
+	//time.Sleep(time.Duration(10) * time.Second)
+	msg := <-messages
+	fmt.Println("UN: " + msg)
+	//time.Sleep(time.Duration(3) * time.Second)
+	msg2 := <-messages
+	fmt.Println("DOS: " + msg2)
+}
+
+func f(from string, mess chan string) {
+	for i := 0; i < 3; i++ {
+		time.Sleep(time.Duration(i) * time.Second)
+		fmt.Println(from, ":", i)
+	}
+	mess <- "resp: " + from
+}
